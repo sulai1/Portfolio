a59dc492dac36979adb19d334425f7bcba46e1d8
Sascha Wernegger <sulai@gmx.at>
2018-06-15 01:58:31 +0200

    Add project files.

---
+++ Cheat Sheet/Controllers/CheatSheetController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cheat_Sheet.Controllers
+{
+    public class CheatSheetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
