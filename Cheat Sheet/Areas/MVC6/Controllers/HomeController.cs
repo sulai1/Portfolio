a2ae969dadff868e83f59b4d7bd404bd4944744c
Sascha Wernegger <sulai@gmx.at>
2018-06-15 01:58:31 +0200

    Add project files.

---
+++ Cheat Sheet/Areas/MVC6/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cheat_Sheet.Areas.MVC6.Controllers
+{
+    [Area("MVC6")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Cheat Sheet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cheat_Sheet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
